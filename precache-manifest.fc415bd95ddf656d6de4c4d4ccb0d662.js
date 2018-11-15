cb6607397f2ab48063cac978a490adade1572452
Bogdan Tudos <bogdantudos@gmail.com>
2018-11-15 00:06:05 -0800

    Updates

---
+++ precache-manifest.fc415bd95ddf656d6de4c4d4ccb0d662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60ebdd6b1870a2709c31",
+    "url": "/personal-site/static/js/runtime~main.60ebdd6b.js"
+  },
+  {
+    "revision": "6fcdb9796db4d0ed3236",
+    "url": "/personal-site/static/js/main.6fcdb979.chunk.js"
+  },
+  {
+    "revision": "ee528a3dbe061229f076",
+    "url": "/personal-site/static/js/1.ee528a3d.chunk.js"
+  },
+  {
+    "revision": "6fcdb9796db4d0ed3236",
+    "url": "/personal-site/static/css/main.fc34c95d.chunk.css"
+  },
+  {
+    "revision": "ee528a3dbe061229f076",
+    "url": "/personal-site/static/css/1.ff17248b.chunk.css"
+  },
+  {
+    "revision": "7b4e6616562c073ff892d39ff2440898",
+    "url": "/personal-site/index.html"
+  }
+];
