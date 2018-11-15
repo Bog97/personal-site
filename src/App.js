7868151b7ce15f3632a09973f637ed6f6637cc73
Bogdan Tudos <bogdantudos@gmail.com>
2018-11-14 23:31:25 -0800

    Complete ps-7 code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { Projects } from "./Projects";
+import Homepage from "./Homepage";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return (<div>
+      <Router>
+        <>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/projects">Projects</Link>
+          </nav>
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route exact path="/" component={Homepage} />
+        </>
+      </Router>
+    </div>
     );
   }
+}
+
+function About() {
+  return <>
+  <h1 className="mb-5"><em>ABOUT ME:</em></h1>
+  <ul className="list">
+    <li>I like web development and am enjoying this course, but it can be quite time consuming</li>
+    <li>I am a senior in Electrical Engineering here at UW and am excited to graduate</li>
+    <li>I <strong>really</strong> want to learn more server side development after this course</li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li>That is all</li>
+  </ul>
+  </>
 }
 
 export default App;
---
+++ src/Homepage.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Homepage extends Component {
+    render() {
+        return (
+            <>
+                <h1>Welcome to my Website!</h1>
+                <p>This is an assignment that shows I know how to use React-Router!</p>
+                <p>Press on the links above to view more information about me.</p>
+            </>
+        )
+    }
+}
+
+export default Homepage;
